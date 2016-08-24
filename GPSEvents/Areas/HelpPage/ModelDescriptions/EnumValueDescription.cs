48b65c89baa7e44a798fba824dcb0de1cd95092e
Oscar_Legorreta <fermosh@gmail.com>
2016-08-23 22:23:31 -0700

    Initial commit

---
+++ GPSEvents/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace GPSEvents.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
